78985670ca9cc57e8fbed34f5b7c0ea6908035c9
TwoPizza <radmir.yakupov94@mail.ru>
2026-08-21 18:24:19 +0300

    Add models, a validator, and tests.

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,44 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+    private final Map<Integer, Film> films = new HashMap<>();
+    private final AtomicInteger idGenerator = new AtomicInteger(1);
+
+    @GetMapping
+    public Collection<Film> getAll() {
+        return films.values();
+    }
+
+    @PostMapping
+    public Film addFilm(@Valid @RequestBody Film film) {
+        int id = idGenerator.getAndIncrement();
+        film.setId(id);
+        films.put(id, film);
+        log.info("Добавлен фильм: {}", film);
+        return film;
+    }
+
+    @PutMapping("/{id}")
+    public Film updateFilm(@PathVariable int id, @Valid @RequestBody Film film) {
+        if (!films.containsKey(id)) {
+            log.warn("Ошибка изменения фильма: {}", film);
+            throw new ValidationException("Фильм с id " + id + " не найден");
+        }
+        film.setId(id);
+        films.put(id, film);
+        log.info("Обновлён фильм: {}", film);
+        return film;
+    }
 }
