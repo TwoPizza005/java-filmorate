1e9ba22c3778a068dcc4432141484a2c4df18f2a
TwoPizza <radmir.yakupov94@mail.ru>
2026-09-19 22:52:29 +0300

    Перенос храненеия в БД

--- src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/friendship/FriendshipStorage.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 public interface FriendshipStorage {
     void addFriend(int userId, int friendId);
+
     void removeFriend(int userId, int friendId);
+
     List<User> getFriends(int userId);
+
     List<User> getCommonFriends(int userId, int otherId);
 }
